648df44e03064ad2af2f01eab7ebbadd3cb1a41c
anjwsm <anjalikushwahawsm@gmail.com>
2023-10-15 19:15:22 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Card(props){
+  return(
+    <div className='my-style'>
+      <h2>{props.name}</h2>
+      <img src={props.img} alt="avatar_img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ReactDOM.render(
+    <div>
+      <h1>My Contacts</h1>
+      <Card name="Beyonce" 
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+91 34553 78951"
+        email="by@once.com"
+      />
+      <Card name="Jack Bauer" 
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+91 34567 98765"
+        email="jack@nowhere.com"
+      />
+      <Card name="Chuck Norris" 
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="+91 98765 12345"
+        email="gmail@chucknorris.com"
+      />
+
+    </div>,
+     document.getElementById("root")
+     );
